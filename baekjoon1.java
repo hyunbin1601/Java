5ee75527c4cafff73ac9c9d7829d546a61314d3b
hyunbin1601 <vina1601@naver.com>
2023-03-09 18:13:46 +0900

    first commit

---
+++ baekjoon1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * baekjoon1
+ */
+public class baekjoon1 {
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
+        int A = kbd.nextInt();
+        int B = kbd.nextInt();
+        System.out.println(A-B);
+    }
+
+    
+}
