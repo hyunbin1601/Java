231d1eee4ed5c3869edc5fcbe7feb6a3486b15ec
hyunbin1601 <vina1601@naver.com>
2023-03-17 17:22:05 +0900

    백준

---
+++ baekjoon9.java
@@ -0,0 +1,12 @@
+public class baekjoon9 {
+    public static void main(String[] args) {
+
+        String sb = "          ,r'\"7\n" +
+                    "r`-_   ,'  ,/\n" +
+                    " \\. \". L_r'\n" +  
+                    "   `~\\/\n" +
+                    "      |\n" +
+                    "      |\n"  ;
+        System.out.print(sb);
+    } //시발 개좆병신같은 문제 다시는 안푼다^^백준 망해라~~ 이러고 또 며칠 뒤에 다시 돌아옴
+}
